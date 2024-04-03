1fbcce2fb11b024d0b2e27ea69998e393099071d
daveansh <dave08225@yahoo.com>
2024-04-03 10:41:15 +0530

    Added WASD temp camera movement Added Q/E camera zoom Added variable to Tiles.gd to randomly pick tilemap size

--- Scripts/Tiles.gd
+++ Scripts/Tiles.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
 @export var tile_prefab : PackedScene
-@export var tile_graphics_pixelSize : int = 32
+@export var tile_graphics_pixelSize : int = 32 #This is hacky, find the sprite scale dynamically.
+@export var max_width_viewport : int = 3
+@export var max_height_viewport : int = 5
+
 var current_hover_tile
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	createLevel() # Replace with function body.
-
+	createTileMap()
+	
 func call_mouse_on_tile(on_tile):
 	current_hover_tile = on_tile
 
@@ -21,13 +24,25 @@ func call_area_entered(area, tile):
 	var a = (distance.length() / maxDistance)
 	if a < 1.0:
 		tile.get_node("Sprite2D").modulate.a -= 1.0 - a
+
+
+func createTileMap():
+	var w : int = randi_range(1, max_width_viewport) * get_viewport().size.x
+	var h : int = randi_range(1, max_height_viewport) * get_viewport().size.y
+	var numTilesX : int = w / tile_graphics_pixelSize
+	var numTilesY : int =  h / tile_graphics_pixelSize
+	var start_x : int = -(numTilesX / 2) * tile_graphics_pixelSize
+	var start_y = 0
+	print(numTilesX, " ", numTilesY, " ", start_x, " ", start_y)
+	createLevel(numTilesX, numTilesY, start_x, start_y)
+	
 # Create a level with tile prefabs.
-func createLevel():
-	for i in 20:
-		for j in 55:
+func createLevel(x, y, start_x, start_y):
+	for i in y:
+		for j in x:
 			var tile = tile_prefab.instantiate() as StaticBody2D
-			tile.position.x = tile_graphics_pixelSize * j
-			tile.position.y = tile_graphics_pixelSize * i
+			tile.position.x = start_x + tile_graphics_pixelSize * j
+			tile.position.y = start_y + tile_graphics_pixelSize * i
 			tile.connect("mouse_on_tile", call_mouse_on_tile) #dynamic connection to the mouse on tile event
 			tile.connect("mouse_off_tile", call_mouse_off_tile) #dynamic connection to the mouse off tile event
 			tile.connect("area_entered", call_area_entered)
@@ -35,6 +50,8 @@ func createLevel():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	# When mouse clicked, and it is hovering over a tile then use the selected tool on the tile / tile area.
 	pass
+	# When mouse clicked, and it is hovering over a tile then use the selected tool on the tile / tile area.
+	#if Input.is_action_just_pressed("ui_select"):
+	#	createTileMap()
 
